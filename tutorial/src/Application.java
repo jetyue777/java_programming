3cdb9dbc8364ede19c74e9738fe263eb547d3435
jetyue <jetyue777@gmail.com>
2017-05-11 21:26:30 -0400

    String

---
+++ tutorial/src/Application.java
@@ -0,0 +1,11 @@
+public class Application {
+
+    public static void main(String[] args) {
+        int test = 7;
+        String good = "dsdfsdf";
+        String hi = " hhh";
+        String all = good + hi;
+        System.out.println("Application.main");
+        System.out.println(all);
+    }
+}
