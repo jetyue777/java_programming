c3d0def49df0547c1826429c73ac864883b29613
jetyue <jetyue777@gmail.com>
2017-05-14 10:34:28 -0400

    class method

--- tutorial/src/Application.java
+++ tutorial/src/Application.java
@@ -1,11 +1,27 @@
+class Person {
+    // Instance variables (data or "state")
+    String name;
+    int age;
+
+    // Class can contain 1 Data 2 Subroutines (methods)
+    public void speak() {
+        System.out.println("My name is: " + name + " and my age is: " + age);
+    }
+
+}
+
 public class Application {
 
     public static void main(String[] args) {
-        int test = 7;
-        String good = "dsdfsdf";
-        String hi = " hhh";
-        String all = good + hi;
-        System.out.println("Application.main");
-        System.out.println(all);
+        Person person1 = new Person();
+        person1.age = 26;
+        person1.name = "Jet";
+
+        person1.speak();
+
+        Person person2 = new Person();
+        person2.age = 24;
+        person2.name = "Renee";
+        person2.speak();
     }
 }
